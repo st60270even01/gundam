840dc8c4fc08db13a555bb9678afcd0c2375e2a7
saru <st60270even01@gmail>
2021-09-13 14:53:42 +0800

    dashboard_product

---
+++ src/components/pages/Login.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <form class="form-signin" @submit.prevent="signin">
+      <h1 class="h3 mb-3 fw-normal">請先登入</h1>
+
+      <div class="form-floating">
+        <label for="floatingInput">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          v-model="user.username"
+        />
+      </div>
+      <div class="form-floating">
+        <label for="floatingPassword">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          v-model="user.password"
+        />
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">
+        Sign in
+      </button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    signin() {
+      const api = `${process.env.APIPATH}/admin/signin`;
+      const vm = this;
+      this.$http.post(api, vm.user).then((response) => {
+        console.log(response.data);
+        if (response.data.success) {
+          const token = response.data.token;
+          const expired = response.data.expired;
+          // console.log(token, expired);
+          document.cookie = `SaruToken=${token}; expires=${new Date(expired)};`;
+          vm.$router.push("/admin/products");
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
+
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import Login from '@/components/pages/Login'
+import Products from '@/components/pages/Products'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+      path: '*',
+      redirect: 'login'
+    },
+    {
+      path: '/login',
+      name: 'Loginin',
+      component: Login
+    },
+    {
+      path: '/admin',
+      name: 'Dashboard',
+      component: Dashboard,
+      children: [{
+        path: 'products',
+        name: 'Products',
+        component: Products,
+        meta: {
+          requiresAuth: true
+        },
+      }]
+    }
+  ]
+})
